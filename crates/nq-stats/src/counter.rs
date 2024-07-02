2591dea80afba362ad8319dc3a28e6c0bf1e02d7
Fisher Darling <fisher@cloudflare.com>
2024-07-02 16:25:37 +0200

    Fix counter integration tests

--- crates/nq-stats/src/counter.rs
+++ crates/nq-stats/src/counter.rs
@@ -102,10 +102,6 @@ mod tests {
 
     use crate::{counter::CounterSeries, instant_minus_intervals};
 
-    fn avg_first_n(n: f64) -> f64 {
-        (n + 1.0) / 2.0
-    }
-
     #[test]
     fn average_simple() {
         let mut ts = CounterSeries::new();
@@ -133,11 +129,11 @@ mod tests {
         }
 
         let total_avg = ts.average().unwrap();
-        assert_eq!(total_avg, avg_first_n(10.0));
+        assert_eq!(total_avg, 1.0);
 
         let to = start + interval_length * 10 + Duration::from_millis(1);
         let from = instant_minus_intervals(to, intervals, interval_length);
         let interval_avg = ts.interval_average(from, to).unwrap();
-        assert_eq!(interval_avg, 8.5); // (10 + 9 + 8 + 7) / 4
+        assert_eq!(interval_avg, 1.0); // (10 - 6) / 4
     }
 }
